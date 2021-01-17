0eb5272ea74e881045f80b08a831e4d26d882d41
hritikchokker <hchokker7@gmail.com>
2021-01-18 00:54:27 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+      <Text>Hello World from react native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F8876F',
     alignItems: 'center',
     justifyContent: 'center',
   },
